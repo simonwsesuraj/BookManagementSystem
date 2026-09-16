0fa86ff9802b21739e726c59ec06f193a57c1f66
simonwsesuraj <simonwsesuraj@gmail.com>
2026-09-16 19:31:41 +0530

    settings for deployement

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -35,7 +35,10 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS =[
+    "localhost:5173",
+    "book-management-system-zeta-nine.vercel.app"
+]
 
 
 # Application definition
